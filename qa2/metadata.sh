2d6987ee74bc8c3ab719af0dcbbfb5914e7b8c21
pandurangareddym <34888203+pandurangareddym@users.noreply.github.com>
2022-05-10 13:01:47 +0530

    json

--- qa2/metadata.sh
+++ qa2/metadata.sh
@@ -2,5 +2,5 @@
 
 for metadata_name in $(Invoke-RestMethod -uri http://169.254.169.254/latest/meta-data/); do
 	echo " $metadata_name values is as follows: "
-	echo "$(curl http://169.254.169.254/latest/meta-data/$metadata_name)"
+	echo "$(curl http://169.254.169.254/latest/meta-data/$metadata_name -H application/json)"
 done	
